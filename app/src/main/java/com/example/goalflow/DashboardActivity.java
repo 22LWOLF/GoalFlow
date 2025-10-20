2bad8128e6e6b0b98a0aba3f0244119a24182727
kylieb233 <s552603@nwmissouri.edu>
2025-10-19 20:54:05 -0500

    Linked OnClick Buttons to Other Activities
    I added the buttons to the code in order for it to switch between activites.

--- app/src/main/java/com/example/goalflow/DashboardActivity.java
+++ app/src/main/java/com/example/goalflow/DashboardActivity.java
@@ -1,6 +1,13 @@
 package com.example.goalflow;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextClock;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,15 +16,63 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DashboardActivity extends AppCompatActivity {
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
+
+        ImageButton achievementsBTN = findViewById(R.id.achievementsBTN);
+        ImageButton settingsBTN = findViewById(R.id.settingsBTN);
+        ImageButton goalCreateBTN = findViewById(R.id.goalCreateBTN);
+        ImageButton trackerBTN = findViewById(R.id.trackerBTN);
+        Image WelcomeImage;
+        TextView welcomeTV;
+        TextClock timeView;
+
+        achievementsBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, AchievementsActivity.class);
+                startActivity(intent);
+            }
+        });
+        settingsBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+        trackerBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, TrackerActivity.class);
+                startActivity(intent);
+            }
+        });
+        goalCreateBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, GoalCreationActivity.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+
+
+
     }
 }
